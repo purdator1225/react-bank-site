92b3267df3eee0d0b22818bdeefc68b2dea2bfec
purdator1225 <briansh1225@gmail.com>
2022-08-25 00:06:19 +0800

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Button from "./Button";
+import Business from "./Business";
+import Client from "./Client";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Button,
+  Business,
+  Client,  
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
